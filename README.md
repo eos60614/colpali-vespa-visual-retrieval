8ce4d255e976fd0f98bfdd64561270623085469a
Thomas Thoresen <thomasht86@users.noreply.huggingface.co>
2024-10-07 12:51:13 +0000

    Upload folder using huggingface_hub

--- README.md
+++ README.md
@@ -1,14 +1,109 @@
 ---
-title: Colpali Vespa Visual Retrieval
-emoji: 👁
-colorFrom: red
-colorTo: red
+title: ColPali 🤝 Vespa - Visual Retrieval
+short_description: Visual Retrieval with ColPali and Vespa
+emoji: 👀
+colorFrom: purple
+colorTo: blue
 sdk: gradio
-sdk_version: 4.44.1
-app_file: app.py
+sdk_version: 4.44.0
+app_file: main.py
 pinned: false
 license: apache-2.0
-short_description: ColPali 🤝 Vespa - Visual Retrieval
+models:
+  - vidore/colpaligemma-3b-pt-448-base
+  - vidore/colpali-v1.2
+preload_from_hub:
+  - vidore/colpaligemma-3b-pt-448-base config.json,model-00001-of-00002.safetensors,model-00002-of-00002.safetensors,model.safetensors.index.json,preprocessor_config.json,special_tokens_map.json,tokenizer.json,tokenizer_config.json 12c59eb7e23bc4c26876f7be7c17760d5d3a1ffa
+  - vidore/colpali-v1.2 adapter_config.json,adapter_model.safetensors,preprocessor_config.json,special_tokens_map.json,tokenizer.json,tokenizer_config.json 9912ce6f8a462d8cf2269f5606eabbd2784e764f
 ---
 
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+<!-- Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root. -->
+
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://assets.vespa.ai/logos/Vespa-logo-green-RGB.svg">
+  <source media="(prefers-color-scheme: light)" srcset="https://assets.vespa.ai/logos/Vespa-logo-dark-RGB.svg">
+  <img alt="#Vespa" width="200" src="https://assets.vespa.ai/logos/Vespa-logo-dark-RGB.svg" style="margin-bottom: 25px;">
+</picture>
+
+# Visual Retrieval ColPali
+
+
+# Developing
+
+First, install `uv`:
+  
+```bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+```
+
+Then, in this directory, run:
+
+```bash
+uv sync --extra dev
+```
+
+This will generate a virtual environment with the required dependencies at `.venv`.
+
+To activate the virtual environment, run:
+
+```bash
+source .venv/bin/activate
+```
+
+And run development server:
+
+```bash
+python hello.py
+```
+
+## Preparation
+
+First, set up your `.env` file by renaming `.env.example` to `.env` and filling in the required values.
+(Token can be shared with 1password, `HF_TOKEN` is personal and must be created at huggingface)
+If you are just connecting to a deployed Vespa app, you can skip to [Connecting to the Vespa app](#connecting-to-the-vespa-app-and-querying).
+
+### Deploying the Vespa app
+
+To deploy the Vespa app, run:
+
+```bash
+python deploy_vespa_app.py --tenant_name mytenant --vespa_application_name myapp --token_id_write mytokenid_write --token_id_read mytokenid_read
+```
+
+You should get an output like:
+
+```bash
+Found token endpoint: https://abcde.z.vespa-app.cloud
+````
+
+### Feeding the data
+
+#### Dependencies
+
+In addition to the python dependencies, you also need `poppler`
+On Mac:
+
+```bash
+brew install poppler
+```
+
+First, you need to create a huggingface token, after you have accepted the term to use the model at https://huggingface.co/google/paligemma-3b-mix-448.
+Add the token to your environment variables as `HF_TOKEN`:
+
+```bash
+export HF_TOKEN=yourtoken
+```
+
+To feed the data, run:
+
+```bash
+python feed_vespa.py --vespa_app_url https://myapp.z.vespa-app.cloud --vespa_cloud_secret_token mysecrettoken
+```
+
+### Connecting to the Vespa app and querying
+
+As a first step, until we hook up to frontend, you can run the `query_vespa.py` script to run some sample queries against the Vespa app:
+
+```bash
+python query_vespa.py
+```
---
+++ main.py
@@ -0,0 +1,152 @@
+import asyncio
+import json
+
+from fasthtml.common import *
+from shad4fast import *
+from vespa.application import Vespa
+
+from backend.colpali import load_model, get_result_from_query
+from backend.vespa_app import get_vespa_app
+from frontend.app import Home, Search, SearchResult, SearchBox
+from frontend.layout import Layout
+
+highlight_js_theme_link = Link(id="highlight-theme", rel="stylesheet", href="")
+highlight_js_theme = Script(src="/static/js/highlightjs-theme.js")
+highlight_js = HighlightJS(
+    langs=["python", "javascript", "java", "json", "xml"],
+    dark="github-dark",
+    light="github",
+)
+
+app, rt = fast_app(
+    htmlkw={"cls": "h-full"},
+    pico=False,
+    hdrs=(
+        ShadHead(tw_cdn=False, theme_handle=True),
+        highlight_js,
+        highlight_js_theme_link,
+        highlight_js_theme,
+    ),
+)
+vespa_app: Vespa = get_vespa_app()
+
+
+class ModelManager:
+    _instance = None
+    model = None
+    processor = None
+
+    @staticmethod
+    def get_instance():
+        if ModelManager._instance is None:
+            ModelManager._instance = ModelManager()
+            ModelManager._instance.initialize_model_and_processor()
+        return ModelManager._instance
+
+    def initialize_model_and_processor(self):
+        if self.model is None or self.processor is None:  # Ensure no reinitialization
+            self.model, self.processor = load_model()
+            if self.model is None or self.processor is None:
+                print("Failed to initialize model or processor at startup")
+            else:
+                print("Model and processor loaded at startup")
+
+
+@rt("/static/{filepath:path}")
+def serve_static(filepath: str):
+    return FileResponse(f"./static/{filepath}")
+
+
+@rt("/")
+def get():
+    return Layout(Home())
+
+
+@rt("/search")
+def get(request):
+    # Extract the 'query' parameter from the URL using query_params
+    query_value = request.query_params.get("query", "").strip()
+
+    # Always render the SearchBox first
+    if not query_value:
+        # Show SearchBox and a message for missing query
+        return Layout(
+            Div(
+                SearchBox(query_value=query_value),
+                Div(
+                    P(
+                        "No query provided. Please enter a query.",
+                        cls="text-center text-muted-foreground",
+                    ),
+                    cls="p-10",
+                ),
+                cls="grid",
+            )
+        )
+
+    # Show the loading message if a query is provided
+    return Layout(Search(request))  # Show SearchBox and Loading message initially
+
+
+@rt("/fetch_results")
+def get(request, query: str, nn: bool = True):
+    # Check if the request came from HTMX; if not, redirect to /search
+    if "hx-request" not in request.headers:
+        return RedirectResponse("/search")
+
+    # Extract the 'query' parameter from the URL
+
+    # Fetch model and processor
+    manager = ModelManager.get_instance()
+    model = manager.model
+    processor = manager.processor
+
+    # Fetch real search results from Vespa
+    result = asyncio.run(
+        get_result_from_query(
+            vespa_app,
+            processor=processor,
+            model=model,
+            query=query,
+            nn=nn,
+            gen_sim_map=True,
+        )
+    )
+
+    # Extract search results from the result payload
+    search_results = (
+        result["root"]["children"]
+        if "root" in result and "children" in result["root"]
+        else []
+    )
+
+    # Directly return the search results without the full page layout
+    return SearchResult(search_results)
+
+
+@rt("/app")
+def get():
+    return Layout(Div(P(f"Connected to Vespa at {vespa_app.url}"), cls="p-4"))
+
+
+@rt("/run_query")
+def get(query: str, nn: bool = False):
+    # dummy-function to avoid running the query every time
+    # result = get_result_dummy(query, nn)
+    # If we want to run real, uncomment the following lines
+    model, processor = get_model_and_processor()
+    result = asyncio.run(
+        get_result_from_query(
+            vespa_app, processor=processor, model=model, query=query, nn=nn
+        )
+    )
+    # model, processor = get_model_and_processor()
+    # result = asyncio.run(
+    #     get_result_from_query(vespa_app, processor=processor, model=model, query=query, nn=nn)
+    # )
+    return Layout(Div(H1("Result"), Pre(Code(json.dumps(result, indent=2))), cls="p-4"))
+
+
+if __name__ == "__main__":
+    # ModelManager.get_instance()  # Initialize once at startup
+    serve()
